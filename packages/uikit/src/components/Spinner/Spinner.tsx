eaf7264700576778b0423076deadfd6448eff30e
cryptoinhero <33366277+cryptoinhero@users.noreply.github.com>
2022-06-02 21:06:54 +0800

    update Spinner

--- packages/uikit/src/components/Spinner/Spinner.tsx
+++ packages/uikit/src/components/Spinner/Spinner.tsx
@@ -5,24 +5,34 @@ import PancakeIcon from "./PancakeIcon";
 import { SpinnerProps } from "./types";
 
 const rotate = keyframes`
-  from {
-    transform: rotate(0deg);
+  0% {
+    transform: transalate(0px, 0px);
   }
-  to {
-    transform: rotate(360deg);
+  25% {
+    transform: translate(40px, 40px);
+  }
+  50% {
+    transform: translate(0px, 0px);
+  }
+  75% {
+    transform: translate(40px, 40px);
+  }
+  100% {
+    transform: translate(0px, 0px);
   }
 `;
 
 const float = keyframes`
-	0% {
-		transform: translatey(0px);
-	}
-	50% {
-		transform: translatey(10px);
-	}
-	100% {
-		transform: translatey(0px);
-	}
+10% { transform: scale(1); }
+20% { transform: scale(1.1); }
+30% { transform: scale(1.2); }
+40% { transform: scale(1.3); }
+50% { transform: scale(1.4); }
+60% { transform: scale(1.5); }
+70% { transform: scale(1.3); }
+80% { transform: scale(1.2); }
+90% { transform: scale(1.1); }
+100% { transform: scale(1); }
 `;
 
 const Container = styled.div`
@@ -33,7 +43,7 @@ const RotatingPancakeIcon = styled(PancakeIcon)`
   position: absolute;
   top: 0;
   left: 0;
-  animation: ${rotate} 2s linear infinite;
+  animation: ${rotate} 4s linear infinite;
   transform: translate3d(0, 0, 0);
 `;
 
@@ -45,7 +55,7 @@ const FloatingPanIcon = styled(PanIcon)`
 const Spinner: React.FC<SpinnerProps> = ({ size = 128 }) => {
   return (
     <Container>
-      <RotatingPancakeIcon width={`${size * 0.5}px`} />
+      <RotatingPancakeIcon width={`${size * 0.2}px`} />
       <FloatingPanIcon width={`${size}px`} />
     </Container>
   );
